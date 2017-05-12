dd233170ac24b5a763f2c53c9529fb16ec8d753e
Aaron Friedman <dubalator@gmail.com>
2017-05-12 12:53:03 -1000

    Implement Scene Transition from BrowseRecipesActvity to ReadRecipeActivity

--- app/src/main/java/com/example/aaron/recipeassistant/RecipeRecyclerViewAdapter.java
+++ app/src/main/java/com/example/aaron/recipeassistant/RecipeRecyclerViewAdapter.java
@@ -2,33 +2,43 @@ package com.example.aaron.recipeassistant;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.aaron.recipeassistant.Model.Meal;
 import com.example.aaron.recipeassistant.Model.MealList;
+import com.example.aaron.recipeassistant.Model.MealToRecipeConverter;
+import com.example.aaron.recipeassistant.Model.Recipe;
 import com.squareup.picasso.Picasso;
 
 public class RecipeRecyclerViewAdapter extends RecyclerView.Adapter<RecipeRecyclerViewAdapter.RecipeViewHolder> {
 
     private Context context;
+    private Activity activity;
     private MealList mealList;
     private int imageSize;
     private Picasso picasso;
 
-    public RecipeRecyclerViewAdapter(@NonNull Activity activity) {
-        this.context = (Context) activity;
+    public RecipeRecyclerViewAdapter(@NonNull Activity activity, int columnCount) {
+        this.context = activity;
+        this.activity = activity;
+
         DisplayMetrics displayMetrics = new DisplayMetrics();
         activity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        imageSize = (displayMetrics.widthPixels / 2);
+        imageSize = (displayMetrics.widthPixels / columnCount);
         picasso = Picasso.with(context);
-        picasso.setIndicatorsEnabled(true);
+        //picasso.setIndicatorsEnabled(true);
     }
 
     void swapMealList(MealList newMealList) {
@@ -46,12 +56,12 @@ public class RecipeRecyclerViewAdapter extends RecyclerView.Adapter<RecipeRecycl
     @Override
     public void onBindViewHolder(RecipeViewHolder holder, int position) {
         Meal meal = mealList.getMeals().get(position);
-        String mealName = meal.getStrMeal();
-        mealName = mealName.toUpperCase();
-        mealName = mealName.replace(" ", "\n");
-        //holder.recipeName.setText(mealName);
-        picasso.load(meal.getStrMealThumb()).noFade().resize(imageSize, imageSize).centerCrop()
+        holder.meal = meal;
+        String mealName = meal.getStrMeal().trim().toUpperCase().replace(" ", "\n");
+        holder.recipeName.setText(mealName);
+        picasso.load(meal.getStrMealThumb()).fit()
                 .into(holder.recipePhoto);
+
 
     }
 
@@ -63,20 +73,40 @@ public class RecipeRecyclerViewAdapter extends RecyclerView.Adapter<RecipeRecycl
 
     class RecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private ImageView recipePhoto;
-        private TextView recipeName;
+        private final ImageView recipePhoto;
+        private final TextView recipeName;
+        private Meal meal;
 
         public RecipeViewHolder(View itemView) {
             super(itemView);
 
             recipePhoto = (ImageView) itemView.findViewById(R.id.recipe_photo);
-            //recipeName = (TextView) itemView.findViewById(R.id.recipe_name);
+            recipeName = (TextView) itemView.findViewById(R.id.recipe_name);
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(imageSize, imageSize);
+            recipeName.setLayoutParams(params);
+            recipePhoto.setLayoutParams(params);
+
             itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
+            MealToRecipeConverter mealToRecipeConverter = new MealToRecipeConverter(meal);
+            Recipe recipe = mealToRecipeConverter.getRecipe();
 
+            Intent intent = new Intent(context, ReadRecipeActivity.class);
+            intent.putExtra("recipe", recipe);
+
+            String transImageName = context.getString(R.string.trans_img);
+            String transTitleName = context.getString(R.string.trans_title);
+
+            Pair<View, String> transImage = Pair.create((View)recipePhoto, transImageName);
+            Pair<View, String> transTitle = Pair.create((View) recipeName, transTitleName);
+
+
+            ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(activity, transImage, transTitle);
+
+            activity.startActivity(intent, options.toBundle());
         }
     }
 }
