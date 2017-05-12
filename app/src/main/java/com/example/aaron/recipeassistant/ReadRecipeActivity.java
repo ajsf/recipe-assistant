fa1b1634fa3eeb3ababcfcdda0b1e2a266546742
Aaron Friedman <dubalator@gmail.com>
2017-05-12 12:50:06 -1000

    Rename MainActivity to ActivityReadRecipe

--- app/src/main/java/com/example/aaron/recipeassistant/ReadRecipeActivity.java
+++ app/src/main/java/com/example/aaron/recipeassistant/ReadRecipeActivity.java
@@ -8,27 +8,25 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.aaron.recipeassistant.Model.InstructionListener;
-import com.example.aaron.recipeassistant.Model.MealList;
-import com.example.aaron.recipeassistant.Model.MealService;
-import com.example.aaron.recipeassistant.Model.Meal;
 import com.example.aaron.recipeassistant.Model.Recipe;
 import com.example.aaron.recipeassistant.Model.RecipeReader;
 import com.example.aaron.recipeassistant.Model.TestRecipeData;
+import com.squareup.picasso.Picasso;
 
-import java.util.List;
+import net.opacapp.multilinecollapsingtoolbar.CollapsingToolbarLayout;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-public class MainActivity extends AppCompatActivity {
+public class ReadRecipeActivity extends AppCompatActivity {
 
     private RecipeReader recipeReader;
 
@@ -36,42 +34,65 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView ingredients;
     private TextView directions;
-    private ImageButton actionListenerToggle;
 
-    private Recipe debugRecipe;
+    private ImageView recipeImage;
+
+    private Recipe recipe;
     private boolean listening;
 
     private AudioManager audioManager;
+
+    private Toolbar toolbar;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+
+    private MenuItem listenerToggle;
 
     private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_read_recipe);
 
         audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
 
         ingredients = (TextView) findViewById(R.id.ingredients_text);
         directions = (TextView) findViewById(R.id.directions_text);
 
+        recipeImage = (ImageView) findViewById(R.id.recipe_header_image);
+
+        recipe = (Recipe) getIntent().getSerializableExtra("recipe");
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+
         initButtons();
         initRecipe();
         listening = false;
+    }
 
-        MealService recipeService = MealService.retrofit.create(MealService.class);
-        Call<MealList> call = recipeService.getRandomSelection();
-        call.enqueue(new Callback<MealList>() {
-            @Override
-            public void onResponse(Call<MealList> call, Response<MealList> response) {
-                Log.i("recipeService Callback", response.toString());
-            }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_read_recipe, menu);
+        listenerToggle = menu.findItem(R.id.action_listen);
+        return true;
+    }
 
-            @Override
-            public void onFailure(Call<MealList> call, Throwable t) {
-                t.printStackTrace();
-            }
-        });
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            supportFinishAfterTransition();
+            return true;
+        }
+        if (item.getItemId() == R.id.action_listen) {
+            toggleListener();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void requestAudioPermission() {
@@ -94,13 +115,14 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
     private void buildReader() {
-        recipeReader = new RecipeReader(MainActivity.this);
-        recipeReader.setRecipe(debugRecipe);
+        recipeReader = new RecipeReader(ReadRecipeActivity.this);
+        recipeReader.setRecipe(recipe);
     }
 
     private void buildListener() {
-        instructionListener = new InstructionListener(MainActivity.this, recipeReader);
+        instructionListener = new InstructionListener(ReadRecipeActivity.this, recipeReader);
         listening = true;
     }
 
@@ -114,8 +136,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initRecipe() {
-        debugRecipe = TestRecipeData.createTestRecipe(this);
-        displayRecipe(debugRecipe);
+        if (recipe == null) {
+            recipe = TestRecipeData.createTestRecipe(this);
+        }
+        displayRecipe(recipe);
     }
 
     private void initButtons() {
@@ -140,42 +164,54 @@ public class MainActivity extends AppCompatActivity {
                 recipeReader.readNextDirection();
             }
         });
-        actionListenerToggle = (ImageButton) findViewById(R.id.action_listener_toggle);
-        actionListenerToggle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (listening) {
-                    actionListenerToggle.setImageResource(R.drawable.ic_microphone_off_black_36dp);
-                    instructionListener.destroy();
-                    listening = false;
-                } else {
-                    actionListenerToggle.setImageResource(R.drawable.ic_microphone_outline_black_36dp);
-                    requestAudioPermission();
-                }
-            }
-        });
+    }
+
+    private void toggleListener() {
+        if (listening) {
+            listenerToggle.setIcon(R.drawable.ic_microphone_off_white_36dp);
+            //actionListenerToggle.setImageResource(R.drawable.ic_microphone_off_black_36dp);
+            instructionListener.destroy();
+            listening = false;
+        } else {
+            listenerToggle.setIcon(R.drawable.ic_microphone_outline_white_36dp);
+            //actionListenerToggle.setImageResource(R.drawable.ic_microphone_outline_black_36dp);
+            requestAudioPermission();
+        }
     }
 
     private void displayRecipe(Recipe recipe) {
+        collapsingToolbarLayout.setTitle(recipe.getTitle().trim().toUpperCase());
+
+        Picasso.with(this).load(recipe.getImageUrl()).fit()
+                .into(recipeImage);
+
+        StringBuilder ingredientsBuilder = new StringBuilder();
+        StringBuilder directionsBuilder = new StringBuilder();
+
         for (String s : recipe.getIngredients()) {
-            ingredients.append(s + "\n");
+            ingredientsBuilder.append(s + "\n");
         }
         for (String s : recipe.getDirections()) {
-            directions.append(s + "\n");
+            directionsBuilder.append(s + "\n\n");
         }
+        int length = directionsBuilder.length();
+        directionsBuilder.delete(length - 2, length);
+        ingredientsBuilder.deleteCharAt(ingredientsBuilder.length() - 1);
+        ingredients.setText(ingredientsBuilder.toString());
+        directions.setText(directionsBuilder.toString());
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i("MainActivity", "OnStart");
+        Log.i("ReadRecipeActivity", "OnStart");
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i("MainActivity", "OnResume");
+        Log.i("ReadRecipeActivity", "OnResume");
         if (recipeReader == null) {
             requestAudioFocus();
         }
@@ -184,12 +220,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i("MainActivity", "OnRestart");
+        Log.i("ReadRecipeActivity", "OnRestart");
     }
 
     @Override
     protected void onDestroy() {
-        Log.i("MainActivity", "onDestroy");
+        Log.i("ReadRecipeActivity", "onDestroy");
         super.onDestroy();
         if (instructionListener != null) {
             instructionListener.destroy();
@@ -200,14 +236,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i("MainActivity", "onStop");
+        Log.i("ReadRecipeActivity", "onStop");
 
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i("MainActivity", "onPause");
+        Log.i("ReadRecipeActivity", "onPause");
     }
 
     private final AudioManager.OnAudioFocusChangeListener audioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
